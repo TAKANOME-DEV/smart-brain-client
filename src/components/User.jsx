9fc8058cf920aca2f463ea57bb372504e86c2648
TAKANOME-DEV <79809121+TAKANOME-DEV@users.noreply.github.com>
2021-10-09 16:28:58 +0000

    update routes

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,6 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
-import { Link, useLocation } from "react-router-dom";
 import { Modal } from ".";
 
 const Container = styled.div`
